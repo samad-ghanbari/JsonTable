600799a38594e30b1ae5d140c9be21b528327220
Samad Ghanbari <ghanbari.samad@gmail.com>
2024-07-22 01:36:54 +0330

    include bold in style

--- jsonTable/jsontable.h
+++ jsonTable/jsontable.h
@@ -35,7 +35,7 @@ class JsonTable : public QObject
     Q_OBJECT
 public:
     explicit JsonTable(double _default_width, double _default_height, QString _default_background_color, QString _default_color, double _default_font_size, QString _default_font_family, QObject *parent = nullptr );
-    QJsonObject createStyle(double _width=0, double _height=0, QString _backgroundColor=NULL, QString _color=NULL, double _fontSize=0, QString _fontFamily=NULL, int rowSpan=0);
+    QJsonObject createStyle(double _width=0, double _height=0, QString _backgroundColor=NULL, QString _color=NULL, double _fontSize=0, bool _bold=false, QString _fontFamily=NULL, int rowSpan=0);
     QJsonObject createObject(QString _type, QString _value, QJsonObject _style);
     QJsonArray createObjects(QString _type, QStringList  _values, QJsonObject _style);
     QJsonArray addObjectToRow(QJsonArray &row, QJsonObject item);
